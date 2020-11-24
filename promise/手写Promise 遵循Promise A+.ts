108c217f8b44ebd799a8a20e7d96ad3de52864d2
wangpeng1 <379522872@qq.com>
2020-11-24 08:57:36 +0800

    10%

--- promise/手写Promise 遵循Promise A+.ts
+++ promise/手写Promise 遵循Promise A+.ts
@@ -1,6 +1,6 @@
-const promise = new Promise((resovle, reject) => {
-  resovle(1);
-})
+// const promise = new Promise((resovle, reject) => {
+//   resovle(1);
+// })
 
 enum PromiseStatus {
   pending = "pending",
@@ -8,21 +8,46 @@ enum PromiseStatus {
   rejected = "rejected",
 }
 
+interface IResolve {
+  (value: any): void;
+}
+
+interface IReject {
+  (reason: any): void;
+}
+
+interface IExcutor {
+  (resolve: IResolve, reject: IReject): void;
+}
+
 class MyPromise {
-  status: PromiseStatus;
-  constructor(excutor) {
-    const resolve = (value: any) => {
+  status = PromiseStatus.pending;
+  value: any;
+  reason: any;
+  constructor(excutor: IExcutor) {
+    const resolve: IResolve = (value: any) => {
       if(this.status === PromiseStatus.pending) { // 只有是pending状态 才能执行， 也意味着promise的状态不可逆
         this.status = PromiseStatus.fulfilled;
+        this.value = value;
       }
     }
 
-    const reject = (reason: any) => {
+    const reject: IReject = (reason: any) => {
       if(this.status === PromiseStatus.pending) { // 只有是pending状态 才能执行， 也意味着promise的状态不可逆
         this.status = PromiseStatus.rejected;
+        this.reason = reason;
+        throw new Error(reason)
       }
     }
 
     excutor(resolve, reject);
   }
 }
+
+const promise = new MyPromise((resolve: IResolve, reject: IReject) => {
+  reject(1);
+});
+
+promise.reason = 2;
+
+console.log(promise);
