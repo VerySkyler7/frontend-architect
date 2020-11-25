bc782183c5a4dbf53caf5782f63d6d539b529150
skyler <379522872@qq.com>
2020-11-25 18:31:31 +0800

    80%

--- promise/MyPromise.ts
+++ promise/MyPromise.ts
@@ -1,5 +1,3 @@
-import { MyPromise } from "./手写Promise 遵循Promise A+";
-
 interface IResolve {
   (value?: any): void;
 }
@@ -12,39 +10,58 @@ interface IExecutor {
   (resolve: IResolve, reject: IReject): void;
 }
 
+interface IFThenCB {
+  (result: any): any;
+}
+
 enum STATE {
   FULFILLED = "fulfilled",
   REJECTED = "rejected",
   PENDING = "pending",
 }
 
+function handlePromise(promise2: Promise, x: Promise, resolve: IResolve, reject: IReject) {
+    if (this.state === STATE.PENDING) {
+      try {
+        if (typeof x === "object" && typeof x.then === "function") {
+          x.then(
+            (y: any) => {
+              this.handlePromise(y, resolve, reject);
+            },
+            (r: any) => {
+              reject(r);
+            }
+          );
+        } else {
+          this.value = x;
+        }
+      } catch (err) {
+        reject(err);
+      }
+    }
+  }
+
 export class Promise {
   value: any = undefined;
   reason: any = undefined;
   state: STATE = STATE.PENDING;
-  onFulfilled: Function = () => { };
-  onRejected: Function = () => { };
+  onFulfilledArr: Function[] = [];
+  onRejectedArr: Function[] = [];
 
   constructor(executor: IExecutor) {
     const resolve: IResolve = (value: any) => {
-      setTimeout(() => {
-        if (this.state === STATE.PENDING) {
-          this.state = STATE.FULFILLED;
-          this.value = value;
-          this.onFulfilled();
-          // this.fulfilledFns.forEach((fn) => fn(this.value));
-        }
-      })
+      if (this.state === STATE.PENDING) {
+        this.state = STATE.FULFILLED;
+        this.value = value;
+        this.onFulfilledArr.forEach((fn) => fn());
+      }
     };
     const reject: IReject = (reason: any) => {
-      setTimeout(() => {
-        if (this.state === STATE.PENDING) {
-          this.state = STATE.REJECTED;
-          this.reason = reason;
-          this.onRejected();
-          // this.rejectedFns.forEach((fn) => fn(this.reason));
-        }
-      })
+      if (this.state === STATE.PENDING) {
+        this.state = STATE.REJECTED;
+        this.reason = reason;
+        this.onRejectedArr.forEach((fn) => fn());
+      }
     };
     try {
       executor(resolve, reject);
@@ -53,58 +70,104 @@ export class Promise {
     }
   }
 
-  then(onFulfilled?: (res: any) => any, onRejected?: (res: any) => any): Promise { // then只是注册回调，等到resolve或reject的执行
-    // onFulfilled && (this.onFulfilled = onFulfilled);
-    // onRejected && (this.onRejected = onRejected);
-    // return new MyPromise((resolve, reject) => {
-    //   resolve(this.onFulfilled())
-    // })
-    return new Promise((resolve, reject) => {
-      if (onFulfilled) {
-        this.onFulfilled = function () {
-          try {
-            const newValue = onFulfilled(this.value);
-            resolve(newValue);
-          } catch (error) {
-            reject(error);
-          }
+  then(onFulfilled?: IFThenCB, onRejected?: IFThenCB): Promise { // p1
+    const promise2 = new Promise((resolve, reject) => { // p2是p1的then的返回值
+      if (this.state === STATE.FULFILLED) {
+        // 这样做可以确保同一个promise，不同的then在不同时机执行时，确保拿到同一个值
+        try {
+          setTimeout(() => {
+            if(onFulfilled) {
+                const x = onFulfilled(this.value); // p3是p1 then的回调的返回值
+                // 通过p1里onFulfilled返回的p3的状态，来决定p2的状态
+                handlePromise(promise2, x, resolve, reject);
+            }
+          });
+        } catch (err) {
+          reject(err);
         }
       }
-      if (onRejected) {
-        this.onRejected = function () {
-          try {
-            const newValue = onRejected(this.reason);
-            resolve(newValue);
-          } catch (error) {
-            reject(error)
-          }
-        }
-      }
-    })
 
+      if (this.state === STATE.REJECTED) {
+        try {
+          setTimeout(() => {
+            onRejected && onRejected(this.reason);
+          });
+        } catch (err) {
+          reject(err);
+        }
+      }
+
+      if (this.state === STATE.PENDING) {
+        onFulfilled &&
+          this.onFulfilledArr.push(() => {
+            // 此方式为aop 切片，将切片函数进行订阅，aop方便做一些额外的事
+            setTimeout(() => {
+              try {
+                const newValue = onFulfilled(this.value);
+                resolve(newValue);
+              } catch (error) {
+                reject(error);
+              }
+            });
+          });
+        onRejected &&
+          this.onRejectedArr.push(() => {
+            setTimeout(() => {
+              try {
+                const newValue = onRejected(this.reason);
+                resolve(newValue);
+              } catch (error) {
+                reject(error);
+              }
+            });
+          });
+      }
+    });
+
+    return promise2;
   }
 }
 
-const p = new Promise((resolve, reject) => {
-  console.log(1);
-  reject(4)
-  console.log(2);
+const p1 = new Promise((resolve, reject) => {
+  resolve(1);
+})
+
+const p2 = p1.then(val => {
+    const p3 = new Promise((resolve, reject) => {
+        reject(val);
+    })
+    return p3;
+}, val => {
+    
 });
 
-p.then(
+p2.then(
   (res) => {
     console.log(res);
+    console.log(2);
   },
   (res) => {
     console.log(res);
-    throw new Error('5')
+    console.log(3);
   }
-).then((res) => {
-  console.log(res);
-}, (res) => {
-  console.log(res);
-  console.log(6);
-  
-});
+);
 
-console.log(3);
+// Promise constructor 规范
+// 1. 构造函数里的代码为同步代码
+// 2. promise的初始状态为pending，当触发回调resolve，状态变为fulfilled，当触发回调reject或抛出异常时，状态变为rejected
+// 3. 当状态发生变化后，状态不可逆
+// 4. 构造器里的回调executor遇到异常时，会被构造器捕获，不会中断程序的运行。
+// 5. 构造器里的executor回调中的resolve或reject的内部执行为异步
+// 6. resolve、reject接受的参数可以是包含promise的任意值
+
+// promise then 规范
+// 0. then方法返回的是一个新的promise
+// 1. 无论在onFulfilled还是onRejected的回调中，返回的是非promise的值，则下一个then的onFulfilled会接受到此非Promise的值
+// 2. 如果then的回调里，出现了异常，则会触发下一个then的onRejected
+// 3. 如果then的回调里返回的是promise：
+//  3.1 promise的状态为fulfilled，则会触发下一个then的onFulfilled。
+//  3.2 promise的状态为rejected，则会触发下一个then的onRejected。
+// 4. 一个promise可以挂载多个不同then的回调,每个then里的回调拿到promise的value或reason都是同一个
+// 5. then里的onFulfilled和onRejected返回的值可以是包含promise的任意值
+
+// promise
--- promise/test.ts
+++ promise/test.ts
@@ -1,18 +1,11 @@
-export function test () {
+export default (() => {
     const p = new Promise((resolve, reject) => {
-        resolve(1)
+        resolve(Promise.reject(2))
     })
     
-    p.then(()=>{}, (err) => {
-        console.log(err);
+    p.then((res)=>{
+        console.log(res, '******');
+    }, (err) => {
+        console.log(err, '------');
     })
-}
-
-test();
-
-// 实现promise的功能
-// 1. 正常运行构造函数
-// 2. 创建状态，执行resolve reject，改变状态，状态不可逆
-// 3. 如果构造函数运行遇到异常，捕获异常，交给reject, 让promise不影响其他流程
-// 4. 实现then then的特点如下
-// 4.1 resolve或reject的内部执行需要为异步，这样才能将then的回调订阅到promise中，将resolve或reject的值传递给then的回调
+})()
