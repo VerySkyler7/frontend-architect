71f44712907a07c9cafcb8932fab51fcaf128705
skyler <379522872@qq.com>
2020-12-04 10:24:32 +0800

    增加错误处理

--- 3.generator和async、await/2.co实现原理.js
+++ 3.generator和async、await/2.co实现原理.js
@@ -1,10 +1,17 @@
-const fs = require('fs').promises;
+const fs = require("fs").promises;
 
 function* readFile() {
-	const path = require('path')
-	const name = yield fs.readFile(path.resolve(__dirname, './name.txt'), 'utf-8')
-	const age = yield fs.readFile(path.resolve(__dirname, name), 'utf-8')
-	return age;
+  try {
+    const path = require("path");
+    const name = yield fs.readFile(
+      path.resolve(__dirname, "./name.txt" + 1),
+      "utf-8"
+    );
+    const age = yield fs.readFile(path.resolve(__dirname, name), "utf-8");
+    return age;
+  } catch (err) {
+    console.log(err, 88888);
+  }
 }
 
 // 初始解决方式
@@ -23,27 +30,32 @@ function* readFile() {
 
 // 实现co的逻辑就是对初始化方式进行递归
 function myCo(genFn) {
-	return new Promise((resolve, reject) => {
-		try {
-			const it = genFn();
+  return new Promise((resolve, reject) => {
+    try {
+      const it = genFn();
 
-			function recCo(lastval) {
-				const { value, done } = it.next(lastval);
-				if (done) {
-					resolve(value);
-				} else {
-					Promise.resolve(value).then(val => {
-						recCo(val);
-					})
-				}
-			}
+      function recCo(lastval) {
+        const { value, done } = it.next(lastval);
+        if (done) {
+          resolve(value);
+        } else {
+          Promise.resolve(value).then(
+            (val) => {
+              recCo(val);
+            },
+            (err) => {
+              it.throw("generator 报错啦");
+            }
+          );
+        }
+      }
 
-			recCo();
-		} catch (e) {
-			reject(e);
-		}
-	})
+      recCo();
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
 
 const res = myCo(readFile);
-res.then(res => console.log(res))
+res.then((res) => console.log(res)).catch((e) => console.log("出错了"));
