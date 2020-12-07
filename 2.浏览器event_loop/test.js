31142f47ed22381dc69ca4692963653ff5fa819b
skyler <379522872@qq.com>
2020-12-07 16:05:12 +0800

    更新then微任务执行顺序练习题

--- 2.浏览器event_loop/test.js
+++ 2.浏览器event_loop/test.js
@@ -1,21 +1,18 @@
 Promise.resolve()
 .then(() => {
-	Promise.resolve()
-	.then(() => {
-		console.log(1)
-		return Promise.resolve();  // then 
-	}).then().then().then(() => {
-		console.log(2)
-	})
-}).then(() => {
-	console.log(3);
-}).then(() => {
-	console.log(4);
-}).then(()=>{
-	console.log(5);
-}).then(()=>{
-	console.log(6);
-})
 
-// 队列：[ 1 , 3, 空then, 4, 空then, 5, 2, 6]
-// 输出：1 3 4 5 2 6
+    Promise.resolve()
+    .then(() => console.log(1))
+    .then(() => Promise.resolve())
+    .then(() => console.log(2))
+
+    return Promise.resolve()
+           .then(() => console.log(3))
+           .then(() => console.log(4))
+
+})
+.then(() => console.log(5))
+.then(() => console.log(6))
+
+// 队列： [1 3 空then 4 空then 5 2 6]
+// 打印：1 3 4 5 2 6
