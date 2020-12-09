af40bab000f10225b6dcef7b6e67aae87b58fbe8
skyler <379522872@qq.com>
2020-12-09 19:28:15 +0800

    pratice io api

--- 6.buffer&fs&io流/io流/test/test.js
+++ 6.buffer&fs&io流/io流/test/test.js
@@ -1,10 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 
-const alloc = Buffer.alloc(3);
-fs.open(path.resolve(__dirname, 'a.txt'), 'r', (err, fd) => {
-    fs.read(fd, alloc, 0, 3, 0, (err, bytesRead, buffer) => {
-        console.log(bytesRead);
-        console.log(buffer);
+const res = fs.createReadStream(path.resolve(__dirname, 'a.txt'), {
+    flags: 'r',
+    autoClose: true,
+    highWaterMark: 3,
+    start: 0,
+    end: 11,
+})
+
+const rw = fs.createWriteStream(path.resolve(__dirname, 'b.txt'), {
+    flags: 'w',
+    emitClose: true,
+    start: 0,
+    highWaterMark: 1
+})
+
+res.on('data', function(data) {
+    rw.write(data, 'utf-8', (err) => {
+        err && console.log(err);
     })
 })
+
