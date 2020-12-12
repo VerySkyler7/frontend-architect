b1eecfc222c8bebf28f7de554214e17a94be6aef
skyler <379522872@qq.com>
2020-12-12 14:16:52 +0800

    可读流100%

--- 6.buffer&fs&io流/io流/ReadStream/ReadStream源码实现.js
+++ 6.buffer&fs&io流/io流/ReadStream/ReadStream源码实现.js
@@ -5,10 +5,13 @@ const EE = require('events')
 /**
  * 实现思路
  * 1. 继承events，能够使用订阅发布 open data end等
- * 2. 参数默认初始化
- * 3. 调用open打开文件,emit open方法
- * 4. 调用open后立马调用_read读取文件
- * 5. 根据highWaterMark、start、end、bytesRead递归读取文件
+ * 2. 构造函数中：
+ *  1. 参数默认初始化
+ *  2. 调用open打开文件,emit open方法
+ *  3. 监听newListener， 当用户订阅data事件时，触发_read方法
+ * 3. 调用_read方法，使用fd进行读取文件。由于open的回调为异步，_read方法第一次拿不到fd，因此可以once一下open方法，当emit open时，重新运行_read即可拿到fd。
+ * 4. 根据highWaterMark、start、end、bytesRead递归读取文件
+ * 5. 当bytesRead为0时，emit end >> fs.close >> emit close
  */
 class ReadStream extends EE{
     constructor(path, options = {}) {
@@ -34,10 +37,15 @@ class ReadStream extends EE{
         if(err) {
             this.emit('error', err);
         }
+        if(typeof this.fd === 'number' && this.autoClose) {
+            fs.close(this.fd, () => {
+                this.emit('close');
+            });
+        }
     }
 
     open() {
-        fs.open(this.path, this.flags, (err, fd) => {
+        fs.open(this.path, this.flags, (err, fd) => { // open 为异步，属于事件环中的poll
             if(err) this.destroy();
             this.fd = fd;
             this.emit('open', this.fd)
@@ -50,7 +58,7 @@ class ReadStream extends EE{
             return this.once('open', () => this._read())
         }
         const buffer = Buffer.alloc(this.highWaterMark);
-        // 如果end-start < hightWaterMark， 则取end-start作为fs.read里的length。
+        // 如果end-start + 1 < hightWaterMark， 则取end-start+1作为fs.read里的length。加1是因为end-start比实际的length少1，所以要加1。
         const howMouchToRead = this.end ? Math.min(this.highWaterMark, this.end - this.offset + 1) : this.highWaterMark;
         fs.read(this.fd, buffer, 0, howMouchToRead, this.offset, (err, bytesRead) => {
             if(bytesRead > 0) {
@@ -60,22 +68,23 @@ class ReadStream extends EE{
                  this._read();
             }else {
                 this.emit('end');
+                this.destroy();
             }
         })
     }
 }
 
-// const res = fs.createReadStream(path.resolve(__dirname, 'test/a.txt                          '), {
+// const res = fs.createReadStream(path.resolve(__dirname, 'test/a.txt'), {
 const res = new ReadStream(path.resolve(__dirname, 'test/a.txt'), {
     flags: 'r',
     autoClose: true,
     highWaterMark: 3, // 分片读取  不填默认为64k
     start: 0, // 开始读的位置
-    end: 3, // 读取结束的位置
+    end: 1, // 读取结束的位置
 })
 
 res.on('open', function(fd) {
-    console.log(fd, 'fd');
+    console.log('fd', fd);
 });
 
 const bufferArr = []
@@ -87,6 +96,6 @@ res.on('end', function() {
     console.log(Buffer.concat(bufferArr).toString()); 
 })
 
-// res.on('close', function(){
-//     console.log('close');
-// })
+res.on('close', function(){
+    console.log('close');
+})
