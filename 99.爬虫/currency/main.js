cb4bc869a65df785cdf9e501c2a2b04ca1fe6faf
wangpeng <wangpeng@xiaoneng.cn>
2021-02-21 20:51:14 +0800

    修改count

--- 99.爬虫/currency/main.js
+++ 99.爬虫/currency/main.js
@@ -1,10 +1,20 @@
 const puppeteer = require('puppeteer');
+const nodemailer = require('nodemailer');
 
 const superArr = {
     binance: [],
     huoBi: []
 };
 
+let transporter = nodemailer.createTransport({
+    host: 'smtp.163.com',
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: 'wangxpengx@163.com', // generated ethereal user
+        pass: 'ONRXIIYHFIVOWVNW' // generated ethereal password
+    }
+});
 
 (async () => {
     const browser = await puppeteer.launch({ headless: true })
@@ -17,7 +27,7 @@ const superArr = {
     await page.evaluate(() => {
         const targetArr = [
             {name: 'BNB', sort: 3, count: 31.4, costPrice: 50},  
-            {name: 'XVS', sort: 4.5, count: 15, costPrice: 87},  
+            {name: 'XVS', sort: 4.5, count: 32.9, costPrice: 87},  
             {name: 'CAKE', sort: 6, count: 35, costPrice: 11.4}
         ];
         setInterval(() => {
@@ -49,7 +59,7 @@ const superArr = {
         const targetArr = [
             {name: 'badger', sort: 0, count: 28, costPrice: 57}, 
             {name: 'dot', sort: 1, count: 2427, costPrice: 4}, 
-            {name: 'ksm', sort: 2, count: 167.75, costPrice: 103}, // 214 * 7
+            {name: 'ksm', sort: 2, count: 160.75, costPrice: 103}, // 214 * 7
             {name: 'mdx', sort: 4, count: 1421, costPrice: 3}, 
             {name: 'xem', sort: 5, count: 1647, costPrice: 0.55},
             {name: 'btc', sort: 7, count: 0, costPrice: 0}, 
@@ -85,6 +95,28 @@ const superArr = {
             total += item.price * item.count;
             return prev + item.name.toLocaleLowerCase() + ':' + item.price + ':' + item.rise + ':' + Number(Number(item.price) * item.count * 6.4).toFixed(2) + '  '
         }, '');
-        if(res) console.log('total:' + Number(total * 6.4).toFixed(2) + ' ' + res)
+        if(res) {
+            console.log('total:' + Number(total * 6.4).toFixed(2) + ' ' + res)
+            // sendMail('total:' + Number(total * 6.4).toFixed(2) + ' ' + res)
+        }
     }, 1000);
 })();
+
+
+function sendMail(msg) {
+    let mailOptions = {
+        from: 'wangxpengx@163.com', // sender address
+        to: '379522872@qq.com', // list of receivers
+        subject: 'coin', // Subject line
+        html: msg // html body
+    };
+    
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    
+    });
+}
