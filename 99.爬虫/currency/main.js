1774960d40f28584f58b2c1ae2da2016cdaa70b7
PenyWang <379522872@qq.com>
2021-07-29 14:53:31 +0800

    add feixiaohao & coinmarket

--- 99.爬虫/currency/main.js
+++ 99.爬虫/currency/main.js
@@ -6,6 +6,8 @@ const superData = {
     currentTotal: 0, // 当前总资产
     binance: [],
     huoBi: [],
+    coinmarket: [],
+    feixiaohao: [],
     temp: [
         // {name: 'bscx', sort: 10, count: 200, costPrice: 18.5, price:  14.76}
     ]
@@ -41,7 +43,7 @@ const superData = {
         }, 1000);
     });
 
-})();
+});
 
 // 定时爬取火币网数据
 (async () => {
@@ -76,13 +78,109 @@ const superData = {
         }, 1000);
     });
 
+});
+
+// 定时爬取coinmarketcap
+(async () => {
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage();
+    await page.goto('https://coinmarketcap.com/', { timeout: 9999999 });
+
+    page.on('console', msg => {
+        if (msg._type === 'info') superData.coinmarket = JSON.parse(msg._text)
+    })
+
+    await page.evaluate(() => {
+        const targetArr = [
+            { name: 'btc', fullName: 'bitcoin', sort: 0, count: 0, costPrice: 0 },
+            { name: 'cake', fullName: 'pancakeswap', sort: 1, count: 5000, costPrice: 12.1 },
+            { name: 'dot', fullName: 'polkadot-new', sort: 2, count: 2450, costPrice: 4 },
+            { name: 'bnb', fullName: 'binance-coin', sort: 3, count: 0, costPrice: 50 },
+            { name: 'matic', fullName: 'polygon', sort: 4, count: 0, costPrice: 0 },
+            { name: 'ksm', fullName: 'kusama', sort: 5, count: 0, costPrice: 103 },
+            { name: 'eth', fullName: 'ethereum', sort: 6, count: 0, costPrice: 1840 },
+            { name: 'doge', fullName: 'dogecoin', sort: 7, count: 0, costPrice: 0 },
+            { name: 'uni', fullName: 'uniswap', sort: 8, count: 0, costPrice: 0 },
+        ];
+        setInterval(() => {
+            const res = targetArr.reduce((prev, item) => {
+                let price = "";
+                let rise = "";
+                const elem = document.querySelector(`a[href="/currencies/${item.fullName}/"]`);
+                if(!elem) return prev;
+                const firstParent = elem.parentElement;
+                firstParent.tagName.toUpperCase() == 'TD' ? 
+                    price = elem.parentElement.nextSibling.firstChild.innerText.match(/[\d|,|\.]+/g)[0] : 
+                    price = elem.parentElement.parentElement.nextSibling.firstChild.firstElementChild.innerText.match(/[\d|,|\.]+/g)[0];
+                firstParent.tagName.toUpperCase() == 'TD' ? 
+                    rise = elem.parentElement.nextSibling.nextSibling.firstChild.innerText :
+                    rise = elem.parentElement.parentElement.nextSibling.nextSibling.firstChild.innerText;
+                price = price.replaceAll(",", "");
+                prev.push({ ...item, price, rise });
+                console.debug(item.name, price)
+                return prev
+            }, []);
+            console.info(JSON.stringify(res))
+        }, 1000);
+    });
+
+});
+
+// 定时爬取非小号数据
+(async () => {
+    const browser = await puppeteer.launch({ headless: true })
+    const page = await browser.newPage();
+    await page.goto('https://www.feixiaohao.com/', { timeout: 9999999 });
+
+    page.on('console', msg => {
+        if (msg._type === 'info') superData.feixiaohao = JSON.parse(msg._text)
+    })
+
+    await page.evaluate(() => {
+        /** 切换成美元 */
+        document.querySelectorAll('.ivu-dropdown-menu')[2].firstElementChild.nextElementSibling.click();
+
+        const targetArr = [
+            { name: 'btc', fullName: 'bitcoin', sort: 0, count: 0, costPrice: 0 },
+            { name: 'cake', fullName: 'pancakeswap', sort: 1, count: 5000, costPrice: 12.1 },
+            { name: 'dot', fullName: 'polkadot100', sort: 2, count: 2450, costPrice: 4 },
+            { name: 'bnb', fullName: 'binance-coin', sort: 3, count: 0, costPrice: 50 },
+            { name: 'matic', fullName: 'polygon', sort: 4, count: 0, costPrice: 0 },
+            { name: 'ksm', fullName: 'kusama', sort: 5, count: 0, costPrice: 103 },
+            { name: 'eth', fullName: 'ethereum', sort: 6, count: 0, costPrice: 1840 },
+            { name: 'doge', fullName: 'dogecoin', sort: 7, count: 0, costPrice: 0 },
+            { name: 'uni', fullName: 'uniswap', sort: 8, count: 0, costPrice: 0 },
+        ];
+        setInterval(() => {
+            const res = targetArr.reduce((prev, item) => {
+                const elem = document.querySelector(`a[href="/currencies/${item.fullName}/"]`);
+                if(!elem) return prev;
+                const price = elem.parentElement.parentElement.nextElementSibling.nextElementSibling.innerText
+                const rise = elem.parentElement.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.innerText;
+                // let index = 0;
+                // let targetElm = elem.parentElement.parentElement;
+                // while(index < 6) {
+                //     targetElm = targetElm.nextElementSibling;
+                //     index++;
+                // }
+                // const rise = targetElm.innerText;
+                prev.push({ ...item, price, rise });
+                console.debug(item.name, price)
+                return prev
+            }, []);
+            console.info(JSON.stringify(res))
+        }, 1000);
+    });
+
 })();
 
+// 有的价格没变动
 
 // 定时计算爬取后的数据
 (() => {
     setInterval(() => {
-        let arr = superData.huoBi.concat(superData.binance).concat(superData.temp);
+        // let arr = superData.huoBi.concat(superData.binance).concat(superData.temp);
+        let arr = superData.feixiaohao;
         if (arr.length > 8) {
             arr = arr.sort((a, b) => a.sort - b.sort)
         }
